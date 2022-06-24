b4758ea9ea9ce0988c02cdf7f2259d0652f1429a
Guthen <33220603+Guthen@users.noreply.github.com>
2022-06-24 13:15:32 +0200

    Re-factor grayscale.glsl to fit code style

--- assets/shaders/grayscale.glsl
+++ assets/shaders/grayscale.glsl
@@ -2,27 +2,20 @@
 
 #version 330
 
-// Input vertex attributes (from vertex shader)
 in vec2 fragTexCoord;
 in vec4 fragColor;
 
-// Input uniform values
 uniform sampler2D texture0;
 uniform vec4 colDiffuse;
 
-// Output fragment color
 out vec4 finalColor;
 
-// NOTE: Add here your custom variables
+//  NTSC conversion weights
+vec3 weights = vec3( .299f, .587f, .114f );
 
 void main()
 {
-    // Texel color fetching from texture sampler
-    vec4 texelColor = texture(texture0, fragTexCoord)*colDiffuse*fragColor;
-
-    // Convert texel color to grayscale using NTSC conversion weights
-    float gray = dot(texelColor.rgb, vec3(0.299, 0.587, 0.114));
-
-    // Calculate final fragment color
-    finalColor = vec4(gray, gray, gray, texelColor.a);
+    vec4 color = texture( texture0, fragTexCoord ) * colDiffuse * fragColor;
+    float gray = dot( color.rgb, weights );
+    finalColor = vec4( gray, gray, gray, color.a );
 }
