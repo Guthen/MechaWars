e4a4b896d4f91e56ac4d36306aac7f72031c43e5
Guthen <33220603+Guthen@users.noreply.github.com>
2022-07-31 18:07:30 +0200

    [added] `UnitState_Attack`

--- src/game/units/states/unit_state.fwd.h
+++ src/game/units/states/unit_state.fwd.h
@@ -1,6 +1,7 @@
 #pragma once
 
 class UnitState;
+class UnitState_Attack;
 class UnitState_Idle;
 class UnitState_Shoot;
 class UnitState_Move;
---
+++ src/game/units/states/unit_state_attack.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "unit_state.h"
+#include "unit_state_shoot.hpp"
+#include "unit_state_move_to.hpp"
+
+#include <memory>
+
+class UnitState_Attack : public UnitState
+{
+private:
+	std::weak_ptr<WorldEntity> target;
+public:
+	UnitState_Attack( Unit* unit, std::weak_ptr<WorldEntity> target ) : UnitState( unit ), target( target ) {};
+
+	void update( float dt ) override
+	{
+		//  check target validity
+		auto target_tmp = target.lock();
+		if ( !target_tmp )
+		{
+			unit->next_state();
+			return;
+		}
+
+		UnitData data = unit->get_data();
+		float dist = utility::distance( unit->get_pos(), target_tmp->get_pos() );
+
+		//  shooter
+		if ( data.shoot.enabled )
+		{
+			//  check target distance
+			if ( dist < data.shoot.min_attack_range )
+			{
+				printf( "too close, move away from target\n" );
+				//  TODO:..
+			}
+			else if ( dist > data.shoot.max_attack_range )
+			{
+				//  move in range
+				printf( "too far, move to target\n" );
+				unit->change_state<UnitState_MoveTo>( true, target, data.shoot.max_attack_range );
+				unit->push_state( true, this );
+			}
+			else
+			{
+				//  shoot
+				printf( "shooting..\n" );
+				unit->change_state<UnitState_Shoot>( true, target );
+				unit->push_state( true, this );
+			}
+		}
+		//  brawler
+		else if ( data.melee.enabled )
+		{
+			//  check target distance
+			if ( dist > data.melee.attack_range )
+			{
+				//  move in range
+				printf( "too far, move to target\n" );
+				unit->change_state<UnitState_MoveTo>( true, target, data.melee.attack_range );
+				unit->push_state( true, this );
+			}
+			else
+			{
+				//  melee
+				printf( "melee..\n" );
+				//unit->change_state<UnitState_Shoot>( true, target );
+				//unit->push_state( true, this );
+			}
+		}
+	}
+
+	void set_target( std::weak_ptr<WorldEntity> _target ) { target = _target; }
+
+	std::string str() const override { return "UnitState_Attack"; }
+};
