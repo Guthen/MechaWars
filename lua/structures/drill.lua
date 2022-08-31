cf40576109621ba048a599653d5d1b04fb854b76
Guthen <33220603+Guthen@users.noreply.github.com>
2022-08-31 21:59:38 +0200

    [added] drill structure

---
+++ lua/structures/drill.lua
@@ -0,0 +1,10 @@
+local DATA = {}
+DATA.health = 500
+DATA.work_to_make = 50
+
+--  renderer
+DATA.texture_path = "assets/textures/structures/drill.png"
+DATA.quad = { 0, 0, 8, 8 }
+DATA.team_quad = { 8, 0, 8, 8 }
+
+define_structure( "drill", DATA )
